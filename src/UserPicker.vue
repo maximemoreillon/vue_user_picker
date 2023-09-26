6f3c2d4dba17fb1f6e464f365ef9920cdc7a39de
Maxime Moreillon <m.moreillon@gmail.com>
2023-09-26 21:38:08 +0900

    search input colors

--- src/UserPicker.vue
+++ src/UserPicker.vue
@@ -1,91 +1,83 @@
 <template>
   <div class="user_picker">
-
     <GroupPicker
       class="picker_container groups_container"
       @selection="get_users_of_group($event)"
       :selectedGroupId="selectedGroupId"
       :groupManagerApiUrl="groupManagerApiUrl"
       :groupManagerFrontUrl="groupManagerFrontUrl"
-      usersWithNoGroup/>
-
+      usersWithNoGroup
+    />
 
     <div class="picker_container users_container">
-
       <template v-if="!users_loading && selected_group && !error">
         <div class="search_wrapper">
-          <input
-            placeholder="Search users"
-            type="text"
-            v-model="search">
-          <MagnifyIcon
-            class="search"/>
+          <input placeholder="Search users" type="text" v-model="search" />
+          <MagnifyIcon class="search" />
         </div>
         <UserList
           :users="filtered_users"
           :selected_group="selected_group"
           :userManagerFrontUrl="userManagerFrontUrl"
-          @selection="$emit('selection',$event)"/>
+          @selection="$emit('selection', $event)"
+        />
       </template>
-
 
       <div
         class="no_selection_wrappper"
-        v-if="!users_loading && !selected_group && !error">
+        v-if="!users_loading && !selected_group && !error"
+      >
         Group not selected
       </div>
 
       <!-- wrapper loader so it can be centered in the div -->
-      <div
-        class="loader_wrapper"
-        v-if="users_loading && !error">
+      <div class="loader_wrapper" v-if="users_loading && !error">
         <loader />
       </div>
 
       <div class="error_wrapper" v-if="!users_loading && error">
         <span class="error">Error while fetching users</span>
       </div>
-
     </div>
-
   </div>
 </template>
 
 <script>
+import axios from "axios"
+import VueCookies from "vue-cookies"
 
-import axios from 'axios'
-import VueCookies from 'vue-cookies'
+import UserList from "./UserList.vue"
 
-import UserList from './UserList.vue'
+import GroupPicker from "@moreillon/vue_group_picker"
+import Loader from "@moreillon/vue_loader"
 
-import GroupPicker from '@moreillon/vue_group_picker'
-import Loader from '@moreillon/vue_loader'
-
-
-import MagnifyIcon from 'vue-material-design-icons/Magnify.vue';
-
-
-
+import MagnifyIcon from "vue-material-design-icons/Magnify.vue"
 
 export default {
-  name: 'UserPicker',
+  name: "UserPicker",
   props: {
     groupManagerApiUrl: {
       type: String,
-      default(){return process.env.VUE_APP_GROUP_MANAGER_API_URL}
+      default() {
+        return process.env.VUE_APP_GROUP_MANAGER_API_URL
+      },
     },
-    groupManagerFrontUrl:  {
+    groupManagerFrontUrl: {
       type: String,
-      default(){return process.env.VUE_APP_GROUP_MANAGER_FRONT_URL}
+      default() {
+        return process.env.VUE_APP_GROUP_MANAGER_FRONT_URL
+      },
     },
     userManagerFrontUrl: {
       type: String,
-      default(){
-        return process.env.VUE_APP_USER_MANAGER_FRONT_URL
-          || process.env.VUE_APP_EMPLOYEE_MANAGER_FRONT_URL
-          || process.env.VUE_APP_GROUP_MANAGER_FRONT_URL
-      }
-    }
+      default() {
+        return (
+          process.env.VUE_APP_USER_MANAGER_FRONT_URL ||
+          process.env.VUE_APP_EMPLOYEE_MANAGER_FRONT_URL ||
+          process.env.VUE_APP_GROUP_MANAGER_FRONT_URL
+        )
+      },
+    },
   },
   components: {
     GroupPicker,
@@ -93,23 +85,23 @@ export default {
     MagnifyIcon,
     Loader,
   },
-  data(){
+  data() {
     return {
       users: [],
       users_loading: false,
       error: null,
       selected_group: undefined,
-      search: '',
+      search: "",
     }
   },
-  mounted(){
-    const jwt = VueCookies.get('jwt')
-    if( jwt && !axios.defaults.headers.common.Authorization){
-      axios.defaults.headers.common['Authorization'] = `Bearer ${jwt}`
+  mounted() {
+    const jwt = VueCookies.get("jwt")
+    if (jwt && !axios.defaults.headers.common.Authorization) {
+      axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`
     }
   },
   methods: {
-    get_users_of_group(group){
+    get_users_of_group(group) {
       this.users_loading = true
       this.selected_group = group
 
@@ -118,47 +110,48 @@ export default {
       const url = `${this.groupManagerApiUrl}/v3/groups/${group_id}/members`
       const params = { batch_size: -1 }
 
-      axios.get(url, {params})
-      .then( ({data}) => { this.users = data.items })
-      .catch(error => {
-        if(error.response) console.error(error.response.data)
-        else console.error(error)
-        this.error = `Error while fetching users`
-      })
-      .finally( () => { this.users_loading = false })
-
+      axios
+        .get(url, { params })
+        .then(({ data }) => {
+          this.users = data.items
+        })
+        .catch((error) => {
+          if (error.response) console.error(error.response.data)
+          else console.error(error)
+          this.error = `Error while fetching users`
+        })
+        .finally(() => {
+          this.users_loading = false
+        })
     },
-
   },
   computed: {
-    filtered_users(){
-      if(!this.search) return this.users
+    filtered_users() {
+      if (!this.search) return this.users
 
       const keys_to_check = [
-        'display_name',
-        'name_kanji',
-        'name',
-        'full_name',
-        'username',
-        'name_romaji',
-        'email_address',
+        "display_name",
+        "name_kanji",
+        "name",
+        "full_name",
+        "username",
+        "name_romaji",
+        "email_address",
       ]
 
-      return this.users.filter(user => {
-
-        return keys_to_check.find( (key) => {
+      return this.users.filter((user) => {
+        return keys_to_check.find((key) => {
           const value = user[key]
-          if(!value) return false
+          if (!value) return false
           return value.toLowerCase().includes(this.search.toLowerCase())
         })
-
       })
     },
-    selectedGroupId(){
-      if(!this.selected_group) return null
+    selectedGroupId() {
+      if (!this.selected_group) return null
       return this.selected_group._id
-    }
-  }
+    },
+  },
 }
 </script>
 
@@ -180,11 +173,10 @@ export default {
   border: 1px solid #dddddd;
 }
 
-.picker_container:last-child{
+.picker_container:last-child {
   margin: 0;
   margin-left: 0.5em;
 }
-
 
 .loader_wrapper {
   height: 100%;
@@ -194,7 +186,7 @@ export default {
   font-size: 200%;
 }
 
-.no_selection_wrappper{
+.no_selection_wrappper {
   height: 100%;
   display: flex;
   align-items: center;
@@ -218,6 +210,8 @@ export default {
 }
 
 .search_wrapper input {
+  background-color: inherit;
+  color: inherit;
   margin-right: 0.5em;
   padding: 0.5em 0.25em;
   flex-grow: 1;
@@ -229,7 +223,7 @@ export default {
   .user_picker {
     flex-direction: column;
   }
-  .picker_container:last-child{
+  .picker_container:last-child {
     margin: 0;
     margin-top: 0.5em;
   }
